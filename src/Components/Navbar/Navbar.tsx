88b4f78e39ef6dcb0f55b52d3802b9091d5d546f
sanjipun22 <iamsanjipun@gmail.com>
2021-07-04 18:44:26 +0545

    Hero Page

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
-import { Box, HStack, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react';
+import { Box, Button, Divider, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, HStack, Input, InputGroup, InputRightElement, Text, useDisclosure, useMediaQuery, VStack } from '@chakra-ui/react';
 import { useHistory } from "react-router-dom";
 import { ReactComponent as Logo } from "Assets/Logo/Logo.svg"
 import React, { useEffect } from 'react';
-import { SearchIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons';
 import { ReactComponent as UserIcon } from "Assets/Icons/User.svg"
 import { ReactComponent as CartIcon } from "Assets/Icons/Cart.svg"
 import { ReactComponent as FavIcon } from "Assets/Icons/Fav.svg"
@@ -35,46 +35,112 @@ const NavigationMenu = [
     },
 ]
 const Navbar: React.FC<NavbarProps> = () => {
-    const history = useHistory()
+    const history = useHistory();
+    const [isMobile] = useMediaQuery("(max-width:540px)");
+    const [isTablet] = useMediaQuery("(max-width:768px)");
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef<any>()
+
     const Products = async () => {
         try {
-            const response = await commerce.products.list()
-            console.log(response.data)
-            console.table(response.data)
+            const response = await commerce.products.list();
+            console.log(response.data);
+            console.table(response.data);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
     useEffect(() => {
         Products()
     }, [])
 
-    return (
-        <Box backgroundColor="#fff" position="fixed" zIndex={1} h={70} top={0} w="100%" d="flex" justifyContent="space-between" alignItems="center" padding="15px 50px">
-            <HStack spacing={9}>
+    const LeftNav = () => {
+        return (
+            <> {!isTablet && <HStack spacing={9}>
                 {NavigationMenu.map((nm: nmTypes) => {
                     return (
                         <Text key={nm.name} onClick={() => history.push(`/${nm.linkTo}`)}>{nm.name}</Text>
                     )
                 })}
-            </HStack>
+            </HStack>}
+                {isTablet && <VStack spacing={5}>
+                    {NavigationMenu.map((nm: nmTypes) => {
+                        return (
+                            <Text key={nm.name} onClick={() => history.push(`/${nm.linkTo}`)}>{nm.name}</Text>
+                        )
+                    })}
+                </VStack>}
+            </>
+        )
+    }
+    const RightNav = () => {
+        return (
+            <>
+                {!isMobile && <HStack margin="0px 20px" spacing={9}>
+                    <InputGroup size="sm">
+                        <Input
+                            pr="4.5rem"
+                            placeholder="Search Product"
+                            maxWidth={200}
+                        />
+                        <InputRightElement width="4.5rem">
+                            <SearchIcon />
+                        </InputRightElement>
+                    </InputGroup>
+                    <UserIcon style={{ width: 30, height: 30 }} />
+                    <Badge count={5}>
+                        <CartIcon style={{ width: 20, height: 20 }} />
+                    </Badge>
+                    <FavIcon style={{ width: 30, height: 30 }} />
+                </HStack>}
+                {isMobile && <VStack margin="0px 20px" spacing={9}>
+                    <InputGroup size="sm">
+                        <Input
+                            pr="4.5rem"
+                            placeholder="Search Product"
+                            maxWidth={200}
+                        />
+                        <InputRightElement width="4.5rem">
+                            <SearchIcon />
+                        </InputRightElement>
+                    </InputGroup>
+                    <UserIcon style={{ width: 30, height: 30 }} />
+                    <Badge count={5}>
+                        <CartIcon style={{ width: 20, height: 20 }} />
+                    </Badge>
+                    <FavIcon style={{ width: 30, height: 30 }} />
+                </VStack>}
+            </>
+        )
+    }
+    return (
+        <Box backgroundColor="#fff" position="fixed" zIndex={1} h={70} top={0} w="100%" d="flex" justifyContent="space-between" alignItems="center" padding="15px 50px">
+            {!isTablet && <LeftNav />}
             <Logo style={{ width: "auto", height: 60 }} />
-            <HStack margin="0px 20px" spacing={9}>
-                <InputGroup size="sm">
-                    <Input
-                        pr="4.5rem"
-                        placeholder="Search Product"
-                    />
-                    <InputRightElement width="4.5rem">
-                        <SearchIcon />
-                    </InputRightElement>
-                </InputGroup>
-                <UserIcon style={{ width: 30, height: 30 }} />
-                <Badge count={5}>
-                    <CartIcon style={{ width: 20, height: 20 }} />
-                </Badge>
-                <FavIcon style={{ width: 30, height: 30 }} />
-            </HStack>
+            {!isMobile &&
+                <RightNav />}
+            {isTablet && <Button variant="primary" ref={btnRef} colorScheme="teal" onClick={onOpen}>
+                <HamburgerIcon />
+            </Button>}
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Navigation Menu</DrawerHeader>
+                    <DrawerBody >
+                        <VStack>
+                            {isMobile && <RightNav />}
+                            <Divider />
+                            <LeftNav />
+                        </VStack>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
         </Box>
     );
 };
--- src/Views/LandingPage/Hero/Hero.tsx
+++ src/Views/LandingPage/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Image, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { Image, SimpleGrid, Text, useMediaQuery, VStack } from '@chakra-ui/react';
 import { ReactComponent as HeroLogo } from "Assets/Logo/HeroLogo.svg"
 import { ReactComponent as CustomDownArrow } from "Assets/Icons/CustomDownArrow.svg"
 import HeroLady from "Assets/Logo/HeroLady.png"
@@ -7,13 +7,15 @@ import React, { useState } from 'react';
 interface HeroProps { }
 
 const Hero: React.FC<HeroProps> = () => {
+    const [isMobile] = useMediaQuery("(max-width:540px)");
+    const [isTablet] = useMediaQuery("(max-width:768px)");
     const [rotate, setRotate] = useState<boolean>(false)
     return (
-        <SimpleGrid position="relative" h={1096} templateColumns="repeat(1,1fr)" backgroundColor="#ED165F">
-            <HeroLogo style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)" }} />
+        <SimpleGrid position="relative" h={isMobile ? 800 : 1096} templateColumns="repeat(1,1fr)" backgroundColor="#ED165F">
+            <HeroLogo style={{ maxWidth: isMobile ? "350px" : isTablet ? "700px" : "100%", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)" }} />
             <Image src={HeroLady} position="absolute" top="50%" left="50%" transform="translate(-50%,-50%)"></Image>
-            <VStack position="absolute" bottom={0} flexDir="column" align="center" left="50%" transform="translate(-50%,-50%)">
-                <Text fontSize={30} color="#fff">Explore Our Collection</Text>
+            <VStack position="absolute" bottom={isMobile ? -30 : 0} flexDir="column" align="center" left="50%" transform="translate(-50%,-50%)">
+                <Text fontSize={30} color="#fff" textAlign="center">Explore Our Collection</Text>
                 <CustomDownArrow style={{ cursor: "pointer", transition: "transform 0.5s ease", transform: rotate ? "rotate(360deg)" : "" }} onMouseEnter={() => setRotate(true)} onMouseLeave={() => setRotate(false)} />
             </VStack>
         </SimpleGrid>
